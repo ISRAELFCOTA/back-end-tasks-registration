24271f715112f93f0fabc8394d34a31730bf2904
ISRAELFCOTA <israelf.cota1203@gmail.com>
2024-02-20 18:53:40 -0300

    style: fix file name entity

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { TasksEntity } from "./mail.entity";
+import { TasksEntity } from "./tasks.entity";
 
 @Module({
   imports: [TypeOrmModule.forFeature([TasksEntity])],
