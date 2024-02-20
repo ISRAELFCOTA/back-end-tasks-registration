ad6b9429c44f932a1d54da55d99c299b8743a59e
ISRAELFCOTA <israelf.cota1203@gmail.com>
2024-02-19 22:23:13 -0300

    feat: typeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true,
+      entities: [__dirname + "/**/*.entity{.js,.ts}"],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
