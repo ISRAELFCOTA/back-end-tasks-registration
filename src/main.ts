4740cfbec333f2f70f203bb134bff167cdfbf105
ISRAELFCOTA <israelf.cota1203@gmail.com>
2024-02-27 21:45:41 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurando o CORS
+  app.enableCors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3030);
 }
 bootstrap();
